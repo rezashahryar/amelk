623b1f1288b0ba4fb905c870dd1b58ec3a04acdc
reza shahryar <reza.shahryarineya7851@gmail.com>
2025-02-03 12:39:07 +0330

    implement profile app

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('', views.profile_index_page_view, name='index'),
     path('favorites/', views.profile_favorite_view, name='favorites'),
+    path('add/house/', views.add_house_case_view, name='add_house'),
+    path('list/house/', views.house_list_view, name='list_house'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -9,3 +9,10 @@ def profile_index_page_view(request):
 
 def profile_favorite_view(request):
     return render(request, 'profiles/profile_favorites.html')
+
+
+def add_house_case_view(request):
+    return render(request, 'profiles/add_house_case.html')
+
+def house_list_view(request):
+    return render(request, 'profiles/house_list.html')
