c6b3bf0e0de51d59fe78b0c513d4273c28579b59
reza shahryar <reza.shahryarineya7851@gmail.com>
2025-02-03 11:51:01 +0330

    implement pages : home , about, faq, privacy, contact_us ( url, view, template )

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('contact-us/', views.ContactUsPageView.as_view(), name='contact'),
+    path('faq/', views.FaqPageView.as_view(), name='faq'),
+    path('privacy/', views.PrivacyPageView.as_view(), name='privacy'),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
 from django.views import generic
 
 # Create your views here.
+
+
+class AboutPageView(generic.TemplateView):
+    template_name = 'pages/about.html'
+
+
+class ContactUsPageView(generic.TemplateView):
+    template_name = 'pages/contact_us.html'
+
+
+class FaqPageView(generic.TemplateView):
+    template_name = 'pages/faq.html'
+
+
+class PrivacyPageView(generic.TemplateView):
+    template_name = 'pages/privacy.html'
